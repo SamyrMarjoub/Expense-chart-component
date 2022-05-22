41ceafb1c2460067e05cd2087b4e3d47210c7de7
Samyr Marjoub <96318513+SamyrMarjoub@users.noreply.github.com>
2022-05-21 21:59:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+$('.mon').hover(function(){
+    $('.mon-span').show()
+    
+})
+$('.mon').mouseout(function(){
+    $('.mon-span').hide()
+})
+
+$('.Tue').hover(function(){
+    $('.tue-span').show()
+    
+})
+$('.Tue').mouseout(function(){
+    $('.tue-span').hide()
+})
+
+$('.wed').hover(function(){
+    $('.wed-span').show()
+    
+})
+$('.wed').mouseout(function(){
+    $('.wed-span').hide()
+})
+
+$('.thu').hover(function(){
+    $('.thu-span').show()
+    
+})
+$('.thu').mouseout(function(){
+    $('.thu-span').hide()
+})
+
+$('.fri').hover(function(){
+    $('.fri-span').show()
+})
+$('.fri').mouseout(function(){
+    $('.fri-span').hide()
+})
+
+$('.sat').hover(function(){
+    $('.sat-span').show()
+    
+})
+$('.sat').mouseout(function(){
+    $('.sat-span').hide()
+})
+
+$('.sun').hover(function(){
+    $('.sun-span').show()
+    
+})
+$('.sun').mouseout(function(){
+    $('.sun-span').hide()
+})
+
+
+
